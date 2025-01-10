f60a647139b27cf0287e1f1353a4d343b7e470bd
Valorx1412 <kama4note141@gmail.com>
2025-01-10 16:46:13 +0800

    delete something

--- components/UserMenu.jsx
+++ components/UserMenu.jsx
@@ -50,17 +50,6 @@ export default function UserMenu({ user }) {
             </div>
           </Link>
 
-          <Link
-            href="/settings"
-            className="block px-4 py-2 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700"
-            onClick={() => setIsOpen(false)}
-          >
-            <div className="flex items-center space-x-2">
-              <FaCog className="w-4 h-4" />
-              <span>Settings</span>
-            </div>
-          </Link>
-
           <button
             onClick={handleSignOut}
             className="block w-full text-left px-4 py-2 text-sm text-red-600 hover:bg-red-50 dark:hover:bg-red-900/30"
